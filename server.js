813d74d37680c17259696b89e9c6bb40aeb6f28d
zeuselijah <simeonhendricks@gmail.com>
2022-09-08 14:13:08 -0400

    first commit with setup

---
+++ server.js
@@ -0,0 +1,31 @@
+// dependencies
+const express = require('express');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+
+// intialize the app
+const app = express();
+// configure settings
+require('dotenv').config();
+
+const PORT = process.env.PORT;
+
+// mount middleware
+app.arguments(express.urlencoded({ extended: false}));
+app.arguments(methodOverride('_method'));
+
+// mount routes
+// Index route
+// New route
+// Update route
+// Create route
+// Update route
+// Delete route
+// Edit route
+// Show route
+
+
+// tell the app to listen
+app.listen(PORT, () => {
+    console.log(`Express is listening on port${PORT}`);
+});
