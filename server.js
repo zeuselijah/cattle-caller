9c3e60b131d2499421be1bc44eae4fcabf639f14
zeuselijah <109929794+zeuselijah@users.noreply.github.com>
2022-09-12 01:48:02 -0400

    Revert "Development"

--- server.js
+++ server.js
@@ -2,44 +2,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
-const cattleRouter = require('./controllers/cattle');
-const expressFileUpload = require('express-fileupload');
-
-
 
 // intialize the app
 const app = express();
-
 // configure settings
 require('dotenv').config();
 
 const PORT = process.env.PORT;
-const DATABASE_URI = process.env.DATABASE_URI;
-
-const db = mongoose.connection;
-
-// connect to mongodb
-mongoose.connect(DATABASE_URI);
-// add mongoDB connected and error event listener
-db.on('connected', () => console.log('Connected to MongoDB'));
-db.on('error', (err) => console.log('MongoDB Error: ' + err.message));
 
 // mount middleware
-
-
-app.use(expressFileUpload({ createParentPath: true }));
-app.use(express.urlencoded({ extended: false}));
-app.use(methodOverride('_method'));
-app.use(express.static('public'));
+app.arguments(express.urlencoded({ extended: false}));
+app.arguments(methodOverride('_method'));
 
 // mount routes
-
-// Homepage redirect route
-app.get('/', (req, res) => res.render('index.ejs'));
-
-
-// mount router/controller
-app.use('/cattle',cattleRouter);
+// Index route
+// New route
+// Update route
+// Create route
+// Update route
+// Delete route
+// Edit route
+// Show route
 
 
 // tell the app to listen
